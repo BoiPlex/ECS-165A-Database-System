a35f03a56374e75fe302360170954fc269ac02dd
Leif Olson <leifolson3@gmail.com>
2025-03-09 23:21:24 -0700

    Improved LockManager explanation

--- lstore/lockmanager.py
+++ lstore/lockmanager.py
@@ -2,8 +2,19 @@ import threading
 
 class LockManager:
     def __init__(self):
-        self.locks = {} # 
+        '''
+        List of locked records. The key should be the RID, with the values being a lock object, list of owners, and lock type.
+        rid: {"lock": threading.Lock(), "owners": set() (Is the set of transaction IDs currently holding the lock, "lock_type": None}
+        '''
+        self.locks = {}
+        
+        '''
+        Creates lock object for self.locks_lock, so that it is locked when get_lock() is called.
+        Can create a bottleneck - to mitigate this we should only lock self.locks when we are modifying (adding/removing locks), not just checking locks.
+        This allows for thread safety and reduces lock contention.
+        '''
         self.locks_lock = threading.lock()
+
 
     """
     The get_lock() function requires a record ID, transaction ID, and lock type.
@@ -16,6 +27,8 @@ class LockManager:
           However, while multiple transactions can hold an "S" lock on a record, no other lock types can be held on that record.
       -> Type "X" is an Exclusive Lock or Write Lock, which only one transactions can hold at a time, as a record is being modified.
       -> Type "U" is an Update Lock, preventing deadlocks. 
+      -> None kind of implies a record is unlocked, but technically it is not if it has transaction owners. If None AND transaction owners is empty
+         then the lock entry should be deleted as there is no transaction holding the record and it is truly unlocked.
       -> Other lock types (which we probably won't use) include 
             --> "IS" (Intention Shared): Transaction intends to acquire "S" locks on specific rows
             --> "IX" (Intention Exclusive): Transaction intends to acquire "X" locks on specific rows
